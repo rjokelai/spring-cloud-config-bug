70e0eb8f7f3c3eb50cc804d02bf720d7b85f08f4
Reko Jokelainen <rjokelainen@pros.com>
2022-03-30 14:10:52 +0300

    output config properties during startup

--- client/src/main/java/com/example/rjokelai/configserverbug/ClientApplication.java
+++ client/src/main/java/com/example/rjokelai/configserverbug/ClientApplication.java
@@ -1,13 +1,32 @@
 package com.example.rjokelai.configserverbug;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ClientTestConfigurationProperties.class)
 public class ClientApplication {
 
+	/**
+	 * Launch with --spring.profiles.active=remote to load from configserver
+	 * Launch with --spring.profiles.active=local to load from local env
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
+	}
+
+	@Autowired
+	public void logConfigProps(ClientTestConfigurationProperties props) throws JsonProcessingException {
+		System.out.println("=============");
+		System.out.println(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(props));
+		System.out.println("=============");
 	}
 
 }
---
+++ client/src/main/java/com/example/rjokelai/configserverbug/ClientTestConfigurationProperties.java
@@ -0,0 +1,31 @@
+package com.example.rjokelai.configserverbug;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RefreshScope
+@ConfigurationProperties(prefix = "client")
+public class ClientTestConfigurationProperties {
+
+  private Map<String, String> mapFromProperties = new HashMap<>();
+  private Map<String, String> mapFromYaml = new HashMap<>();
+
+  public Map<String, String> getMapFromProperties() {
+    return mapFromProperties;
+  }
+
+  public Map<String, String> getMapFromYaml() {
+    return mapFromYaml;
+  }
+
+  public void setMapFromYaml(Map<String, String> mapFromYaml) {
+    this.mapFromYaml = mapFromYaml;
+  }
+
+  public void setMapFromProperties(Map<String, String> mapFromProperties) {
+    this.mapFromProperties = mapFromProperties;
+  }
+}
