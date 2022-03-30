f513ffc2d2a33713e50a43a8e555976d7a757de5
Reko Jokelainen <rjokelainen@pros.com>
2022-03-30 10:51:23 +0300

    initialize projects

---
+++ client/src/test/java/com/example/rjokelai/configserverbug/ConfigServerBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rjokelai.configserverbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
